d8f6ecbe6adbe009aaf66ed7e453dd6cc024e248
Rafa Oliveira <example@noreply.com>
2021-02-11 23:28:29 +0100

    Refactored to have both solutions of part 1 for Day11

--- src/main/kotlin/Day11/Day11Solution.kt
+++ src/main/kotlin/Day11/Day11Solution.kt
@@ -25,6 +25,25 @@ fun Day11Solution() {
     println("Count of #: $count")
 }
 
+fun Day11SolutionPart1Alternative() {
+    println("Day 11 Solution - Part 1 Alternative")
+
+    var seats = Day11Input.input.map { it.toCharArray() }.toTypedArray()
+
+    printMatrix(seats)
+
+    var seatsChanged = true
+    while (seatsChanged) {
+        val (nextIteration, _seatsChanged) = getNextIterationPart1Alternative(seats)
+        seats = nextIteration
+        seatsChanged = _seatsChanged
+    }
+
+    val count = seats.map { it.count { it.equals('#') } }.sum()
+
+    println("Count of #: $count")
+}
+
 fun Day11SolutionPart2() {
     println("Day 11 Solution - Part 2")
 
@@ -58,21 +77,27 @@ fun getNextIterationPart1(matrix: Array<CharArray>): Pair<Array<CharArray>, Bool
         for (j in 0..rightWall) {
             var takenSeats = 0
             val currentSeatState = matrix[i][j]
-            if (currentSeatState == '.') continue
 
-            for(delta_i in -1..1) {
-                for (delta_j in -1..1) {
-                    if (delta_i == 0 && delta_j == 0) continue
+            if (i-1 >= ceiling) {
+                if (matrix[i-1][j] == '#') takenSeats++
 
-                    val adjacentI = i + delta_i
-                    val adjacentJ = j + delta_j
+                if (j-1 >= leftWall && matrix[i-1][j-1] == '#') takenSeats++
 
-                    if ((adjacentI >= ceiling && adjacentI <= floor) && (adjacentJ >= leftWall && adjacentJ <= rightWall)
-                        && matrix[adjacentI][adjacentJ] == '#'
-                    ) {
-                        takenSeats++
-                    }
-                }
+                if (j+1 <= rightWall && matrix[i-1][j+1] == '#') takenSeats++
+            }
+
+            if (j-1 >= leftWall && matrix[i][j-1] == '#') takenSeats++
+
+            if (i+1 <= floor) {
+                if (j-1 >= leftWall && matrix[i+1][j-1] == '#') takenSeats++
+
+                if (matrix[i+1][j] == '#') takenSeats++
+            }
+
+            if (j+1 <= rightWall) {
+                if (i+1 <= floor && matrix[i+1][j+1] == '#') takenSeats++
+
+                if (matrix[i][j+1] == '#') takenSeats++
             }
 
             val newSeatState = when {
@@ -97,8 +122,55 @@ fun getNextIterationPart1(matrix: Array<CharArray>): Pair<Array<CharArray>, Bool
     return Pair(nextIteration, seatsChanged)
 }
 
-// Part 2
+// Part 1 Alternative
+fun getNextIterationPart1Alternative(matrix: Array<CharArray>): Pair<Array<CharArray>, Boolean> {
+    val leftWall = 0
+    val ceiling = 0
+    val rightWall = matrix[0].size - 1
+    val floor = matrix.size - 1
+    var seatsChanged = false
 
+    val nextIteration = matrix.mapIndexed { i, row ->
+        row.mapIndexed { j, currentSeat ->
+            if (currentSeat == '.') currentSeat
+
+            var takenSeats = 0
+            for (delta_i in -1..1) {
+                for (delta_j in -1..1) {
+                    if (delta_i == 0 && delta_j == 0) continue
+
+                    val adjacentI = i + delta_i
+                    val adjacentJ = j + delta_j
+
+                    if ((adjacentI >= ceiling && adjacentI <= floor) && (adjacentJ >= leftWall && adjacentJ <= rightWall)
+                        && matrix[adjacentI][adjacentJ] == '#'
+                    ) {
+                        takenSeats++
+                    }
+                }
+            }
+
+            when {
+                takenSeats == 0 && currentSeat == 'L' -> {
+                    seatsChanged = true
+                    '#'
+                }
+                takenSeats >= 4 && currentSeat == '#' -> {
+                    seatsChanged = true
+                    'L'
+                }
+                else -> currentSeat
+            }
+
+        }.toCharArray()
+    }.toTypedArray()
+
+    println("=============== Changed? $seatsChanged")
+    printMatrix(nextIteration)
+    return Pair(nextIteration, seatsChanged)
+}
+
+// Part 2
 fun getNextIterationPart2(matrix: Array<CharArray>): Pair<Array<CharArray>, Boolean> {
     val nextIteration: Array<CharArray> = matrix.copy()
     val leftWall = 0
@@ -112,6 +184,7 @@ fun getNextIterationPart2(matrix: Array<CharArray>): Pair<Array<CharArray>, Bool
         for (j in 0..rightWall) {
             var visibleTakenSeats = 0
             val currentSeatState = matrix[i][j]
+
             if (currentSeatState == '.') continue
 
             visibleTakenSeats += countTakenSeatInDirection(i, j, -1, 0,
